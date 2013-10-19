b10db2a7322f5bb24f8e38ae7970da573e08667d
caw <christopherwood07@gmail.com>
2013-10-19 16:32:20 -0700

    Whoops

--- compile.sh
+++ compile.sh
@@ -8,7 +8,7 @@ do
 	./clang -o $var.profile $var.profile.s ../lib/libprofile_rt.so
 
 	# Gather the profile data and output, for peace of mind
-	./$var.profile 
+	./$var.profile > $var.profile.out
 	./llvm-prof $var.profile.bc
 
 	### OLD SCRIPT TO RUN ON THE INSTRUMENTED VERSION!
@@ -32,15 +32,15 @@ do
 	./opt -profile-loader -block-placement $var.bc
 	./llc $var.bc -o $var.mod.s
 	./clang -o $var.mod $var.mod.s ../lib/libprofile_rt.so
-	./$var.mod
-	./clang -o $var -c $var.c
+	./$var.mod > $var.mod.out
+	./clang -o $var $var.c
 
 	# Now run the program with the time script and save the output
 	COUNTER=0
 	while [  $COUNTER -lt 5 ]; do
 		echo Running iteration $COUNTER and storing the time
 		perl time.pl $var      > $var.out.$COUNTER
-		perl time.pl $var.mod  > $var.out.$COUNTER
+		perl time.pl $var.mod  > $var.mod.out.$COUNTER
 		let COUNTER=COUNTER+1
 	done
 done
