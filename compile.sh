f76572b4db17e4f639776a17310648ee37f3a890
caw <christopherwood07@gmail.com>
2013-10-19 16:26:31 -0700

    Updated compile script and whatnot

--- compile.sh
+++ compile.sh
@@ -6,9 +6,43 @@ do
     ./opt -insert-edge-profiling $var.bc -o $var.profile.bc
 	./llc $var.profile.bc -o $var.profile.s
 	./clang -o $var.profile $var.profile.s ../lib/libprofile_rt.so
-	./$var.profile
+
+	# Gather the profile data and output, for peace of mind
+	./$var.profile 
 	./llvm-prof $var.profile.bc
-	./opt -profile-loader -block-placement $var.profile.bc
+
+	### OLD SCRIPT TO RUN ON THE INSTRUMENTED VERSION!
+
+	# # Now do the block placement and gather the results...
+	# ./opt -profile-loader -block-placement $var.profile.bc
+	# ./llc $var.profile.bc -o $var.profile_mod.s
+	# ./clang -o $var.profile_mod $var.profile_mod.s ../lib/libprofile_rt.so
+	# ./$var.profile_mod
+
+	# # Now run the program with the time script and save the output
+	# COUNTER=0
+	# while [  $COUNTER -lt 5 ]; do
+	# 	echo Running iteration $COUNTER and storing the time
+	# 	perl time.pl $var.profile     > $var.profile.out.$COUNTER
+	# 	perl time.pl $var.profile_mod > $var.profile.out.$COUNTER
+	# 	let COUNTER=COUNTER+1
+	# done
+
+	# Now do the block placement and gather the results...
+	./opt -profile-loader -block-placement $var.bc
+	./llc $var.bc -o $var.mod.s
+	./clang -o $var.mod $var.mod.s ../lib/libprofile_rt.so
+	./$var.mod
+	./clang -o $var -c $var.c
+
+	# Now run the program with the time script and save the output
+	COUNTER=0
+	while [  $COUNTER -lt 5 ]; do
+		echo Running iteration $COUNTER and storing the time
+		perl time.pl $var      > $var.out.$COUNTER
+		perl time.pl $var.mod  > $var.out.$COUNTER
+		let COUNTER=COUNTER+1
+	done
 done
 
 
