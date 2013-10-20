fa9551bc1b1216aa6e9834689542aef5fb647e3b
chris-wood <christopherwood07@gmail.com>
2013-10-20 11:46:50 -0700

    Pushing new test app for block placement

---
+++ time.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -w
+
+use Time::HiRes qw/ time sleep /;
+
+my $start = time;
+system("./$ARGV[0] > $ARGV[0].out");
+my $end = time;
+my $run_time = $end - $start;
+print "Elapsed time: $run_time\n";
