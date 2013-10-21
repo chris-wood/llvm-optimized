0fb217d05f65e8fb5803d6ec28d1e9ee55a17830
caw <christopherwood07@gmail.com>
2013-10-21 16:37:14 -0700

    Forgot time param for the time script

--- time.pl
+++ time.pl
@@ -3,7 +3,7 @@
 use Time::HiRes qw/ time sleep /;
 
 my $start = time;
-system("./$ARGV[0] > $ARGV[0].out");
+system("./$ARGV[0] $ARGV[1] > $ARGV[0].out");
 my $end = time;
 my $run_time = $end - $start;
 print "Elapsed time: $run_time\n";
